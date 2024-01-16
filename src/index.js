a13be2ee16520b56c3aec53058ae076cbcf75c5e
SERENABACCARI <serena88_2011@libero.it>
2024-01-16 16:58:26 +0100

    ISTALLATO PK

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import apiRouter from '../src/apiRouter.js';
 import mongoose from 'mongoose';
+import list from "express-list-endpoints"
 
 const server = express();
 
@@ -10,12 +11,10 @@ const port = 3030;
 
 server.use("/api", apiRouter);
 
-mongoose.connect("", { 
-    
-   
-}).then(() => {
+mongoose.connect(process.env.MONGO_URL).then(() => {
     server.listen(port, () => {
         console.log('🚀 Server listening to port: ' + port);
+        console.log(list(server.listen));
     });
 }).catch((error) => {
     console.error('Error connecting to the database:', error);
